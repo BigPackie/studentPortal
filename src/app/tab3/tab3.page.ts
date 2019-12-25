a69c0f8f51a559fa67c69c4c07ad49dbd034fce0
Patrik Pindes <ppindes@gmail.com>
2019-12-25 16:10:19 +0700

    visual prototype for rewards with history done

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+enum Mode { 
+  REWARDS = "rewards",
+  HISTORY = "history"
+}
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -9,12 +14,43 @@ export class Tab3Page {
 
   slidingItemOpacity: number = 0;
 
+  mode : Mode = Mode.REWARDS;
+
   constructor() {}
+
+  ionViewDidEnter(){
+    this.updatePageContent();
+  }
 
   drag(eventDetail, item){
     this.slidingItemOpacity = Math.pow(10, eventDetail.ratio*2 - 1) / 10;
     // console.log('dragging:' + JSON.stringify(eventDetail));
     // console.log('item:' + JSON.stringify(item.style));
+  }
+
+  updatePageContent() {
+
+    switch (this.mode) {
+      case Mode.REWARDS:
+        this.loadRewards();
+        break;
+      case Mode.HISTORY:
+        this.loadRewardsHistory();
+        break;
+    
+      default:
+        this.loadRewards();
+        break;
+    }
+
+  }
+
+  loadRewards(){
+    console.log(`loading rewards`);
+  }
+
+  loadRewardsHistory(){
+    console.log(`loading history of rewads`)
   }
 
 }
