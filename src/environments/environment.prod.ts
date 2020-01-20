2d6f2b5dff964474a86c826fcb065e3ead646dbb
Patrik Pindes <ppindes@gmail.com>
2020-01-20 09:38:22 +0700

    ip address change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  newsServicesUrl: "http://10.40.9.80:3000", //TODO: change to correct adress when newsServices finally deployed
+  newsServicesUrl: "http://10.12.4.97:3000", //TODO: change to correct adress when newsServices finally deployed
   rewardServicesUrl: "https://reward.icit.kmutnb.ac.th"
 };
